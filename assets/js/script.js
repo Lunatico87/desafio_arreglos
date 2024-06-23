283d4035ac1c968be8577997db59328f7b4ee500
beto <alberto.luna87@hotmail.com>
2024-06-23 21:35:41 +1200

    correccion de logica homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,110 +1,13 @@
-const propiedades_venta = [
-    {
-    nombre: 'Apartamento de lujo en zona exclusiva',
-    src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
-    descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
-    ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
-    habitaciones: 4,
-    banos: 4,
-    costo: 5000,
-    smoke: false,
-    pets: false
-    },
+import propiedades_venta from "./propiedades_venta.js";
+import propiedades_alquiler from "./propiedades_alquiler.js";
 
-    {
-    nombre: 'Apartamento acogedor en la montaña',
-    src: 'https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg',
-    descripcion: 'Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas',
-    ubicacion: '789 Mountain Road, Summit Peaks, CA 23456',
-    habitaciones: 2,
-    banos: 1,
-    costo: 1200,
-    smoke: true,
-    pets: true
-    },
-
-    {
-    nombre: 'Penthouse de lujo con terraza panorámica',
-    src: 'https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg',
-    descripcion: 'Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares',
-    ubicacion: '567 Skyline Avenue, Skyview City, CA 56789',
-    habitaciones: 3,
-    banos: 3,
-    costo: 4500,
-    smoke: false,
-    pets: true
-    },
-
-    {
-    nombre: 'Casa a metros del lago con vista a las montañas',
-    src: 'https://s.oneroof.co.nz/image/fc/88/fc8802d312c6b5bf487d22c777bed19d.jpg?x-oss-process=image/quality,q_80/resize,w_1080/format,webp',
-    descripcion: '',
-    ubicacion: '16 Panners Way, Queenstown, New Zealand',
-    habitaciones: 6,
-    banos: 5,
-    costo: 1950,
-    smoke: false,
-    pets: true
-    }
-
-    ]
-const propiedades_alquiler = [
-    {
-    nombre: 'Apartamento en el centro de la ciudad',
-    src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60',
-    descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
-    ubicacion: '123 Main Street, Anytown, CA 91234',
-    habitaciones: 2,
-    banos: 2,
-    costo: 2000,
-    smoke: false,
-    pets: true
-    },
-
-    {
-    nombre: 'Apartamento luminoso con vista al mar',
-    src: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-    descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
-    ubicacion: '456 Ocean Avenue, Seaside Town, CA 56789',
-    habitaciones: 3,
-    banos: 2,
-    costo: 2500,
-    smoke: true,
-    pets: true
-    },
-
-    {
-    nombre: 'Condominio moderno en zona residencial',
-    src: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
-    descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencial',
-    ubicacion: '123 Main Street, Anytown, CA 91234',
-    habitaciones: 2,
-    banos: 2,
-    costo: 2200,
-    smoke: false,
-    pets: false
-    },
-
-    {
-    nombre: 'Moderna casa cerca de la gran ciudad',
-    src: 'https://s.oneroof.co.nz/image/17/7a/177a56137693ccc0fa247418ed5d0b1e.jpg?x-oss-process=image/quality,q_80/resize,w_1080/format,webp',
-    descripcion: '',
-    ubicacion: '24 George Deane Place, North Shore City, New Zealand',
-    habitaciones: 6,
-    banos: 3,
-    costo: 1650,
-    smoke: false,
-    pets: false
-    }
-
-    ]
 const venta = document.querySelector("#propiedades-venta");
 const alquiler = document.querySelector("#propiedades-alquiler");
 
-if(venta !== null){
-    let htmlventa = ''
+if(venta !== null ){
+    let html = ''
     for(let propiedad of propiedades_venta){
-    htmlventa += `
+    html += `
     <div class="col-md-4 mb-4">
     <div class="card">
     <img src="${propiedad.src}" class="card-img-top" alt="Imagen del departamento"/>
@@ -120,14 +23,13 @@ if(venta !== null){
     </div>
     </div>
     `;
-       }
-    venta.innerHTML = htmlventa   
+    }
+    venta.innerHTML = html
 }
-
 if(alquiler !== null){
-    let htmlAlquiler = ''
+    let html = ''
     for(let propiedad of propiedades_alquiler){
-    htmlAlquiler += `
+    html += `
     <div class="col-md-4 mb-4">
     <div class="card">
     <img src="${propiedad.src}" class="card-img-top" alt="Imagen del departamento"/>
@@ -143,10 +45,9 @@ if(alquiler !== null){
     </div>
     </div>
     `;
-   }
-alquiler.innerHTML = htmlAlquiler
+    }
+    alquiler.innerHTML = html
 }
-
 if(venta !== null && alquiler !== null){
     let htmlventa = ''
     for(let propiedad of propiedades_venta.slice(0, 3)){
